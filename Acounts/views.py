35edce4340f11c5e4781fdaf7fb10973a3218ed9
Sergio Cusa <sergiocusa.91@gmail.com>
2022-07-17 11:13:23 -0300

    template de login

--- Acounts/views.py
+++ Acounts/views.py
@@ -1,5 +1,28 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate , login as login_funcion
 
 def login (request):
-    return HttpResponse ("")
+    
+    if request.method == "POST":
+        form= AuthenticationForm (request, data=request.POST)
+        if form.is_valid():
+            username= form.cleaned_data.get("username")
+            
+            password= form.cleaned_data.get("password")
+            
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                login_funcion(request,user)
+                
+                return render (request,"inicio.html",{})
+                
+            else:
+                return render (request, "users/login.html" , {"form":form} )    
+        
+        else:
+            return render (request, "users/login.html" , {"form":form} )
+            
+    form= AuthenticationForm ()
+    return render (request, "users/login.html" , {"form":form} )
